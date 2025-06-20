7143e10ec790ccb48883a7c1d4a1359d6e3d4bd4
evgmsk <emoskvich@yandex.ru>
2025-06-20 15:13:46 +0300

    made required imports

---
+++ iam.tf
@@ -0,0 +1,29 @@
+resource "aws_iam_role" "terraform_role" {
+  name = "TerraformRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Federated = "arn:aws:iam::753350392043:oidc-provider/token.actions.githubusercontent.com"
+        }
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+          }
+          StringLike = {
+            "token.actions.githubusercontent.com:sub" = "repo:YOUR_GITHUB_USERNAME/rsschool-devops-course-tasks:*"
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "terraform_role_policy" {
+  role       = aws_iam_role.terraform_role.name
+  policy_arn = "arn:aws:iam::aws:policy/PowerUserAccess"
+}
